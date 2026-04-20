04677c3b10f8b299d37f798a646b94905e0d6bea
Vividh Bhardwaj <61474917+Vividh25@users.noreply.github.com>
2026-04-20 14:49:58 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    int ans = 0;
+    Integer[][] dp;
+    public int change(int amount, int[] coins) {
+        dp = new Integer[coins.length][amount + 1];
+        return recur(coins, amount, 0);
+    }
+
+    public int recur(int[] coins, int amount, int i) {
+        if (amount == 0) {
+            return 1;
+        }
+        if (i == coins.length) {
+            return 0;
+        }
+
+        if (dp[i][amount] != null) return dp[i][amount];
+
+        int ways = 0;
+
+        //pick current coin
+        if (coins[i] <= amount) {
+            ways += recur(coins, amount - coins[i], i);
+        }
+        //dont pick this coin
+        ways += recur(coins, amount, i+1);
+
+        dp[i][amount] = ways;
+        return dp[i][amount];
+    }
+}
