9f61bf4e5dd6551ea8e857dedd57f7f13f05f9a5
Vividh Bhardwaj <61474917+Vividh25@users.noreply.github.com>
2026-05-14 21:00:14 +0530

    Add: triangle - submission-0

---
+++ Data Structures & Algorithms/triangle/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    Integer[][] dp;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        dp = new Integer[triangle.size()][triangle.size()];
+        return recur(triangle, 0, 0);
+    }
+
+    public int recur(List<List<Integer>> triangle, int i, int j) {
+        if (i == triangle.size() || j == triangle.size()) return 0;
+
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        int left = 0, right = 0;
+        left = triangle.get(i).get(j) + recur(triangle, i + 1, j);
+        right = triangle.get(i).get(j) + recur(triangle, i + 1, j + 1);
+
+        // System.out.println(left + " " + right);
+
+        return dp[i][j] = Math.min(left, right);
+    }
+}
