558c25840958b8177dd29565c433f216969f35da
Vividh Bhardwaj <61474917+Vividh25@users.noreply.github.com>
2026-05-20 17:11:47 +0530

    Add: open-the-lock - submission-1

---
+++ Data Structures & Algorithms/open-the-lock/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        if (visited.contains("0000")) return -1;
+        Queue<String> q = new LinkedList<>();
+        q.add("0000");
+        visited.add("0000");
+
+        int turns = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            
+            while (size-- > 0) {
+                String lock = q.poll();
+                if (lock.equals(target)) return turns;
+
+                for (String next : children(lock)) {
+                    if (!visited.contains(next)) {
+                        q.add(next);
+                        visited.add(next);
+                    }
+                }
+            }
+            turns++;
+        }
+        return -1;
+    }
+
+    public List<String> children(String lock) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            char[] arr = lock.toCharArray();
+            arr[i] = (char) (((arr[i] - '0' + 1) % 10) + '0');
+            res.add(new String(arr));
+
+            arr = lock.toCharArray();
+            arr[i] = (char) (((arr[i] - '0' - 1 + 10) % 10) + '0');
+            res.add(new String(arr));
+        }
+        return res;
+    }
+}
