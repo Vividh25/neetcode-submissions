d0ef10b2f693aeb56d3a4dc55b48eff9c78784cd
Vividh Bhardwaj <61474917+Vividh25@users.noreply.github.com>
2026-05-24 17:48:46 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        int[] ans = new int[nums.length - k + 1];
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            pq.offer(new int[]{i, nums[i]});
+
+            while (pq.peek()[0] <= i - k) pq.poll();
+            if (i >= k - 1) ans[idx++] = pq.peek()[1];
+        }
+        return ans;
+    }
+}
